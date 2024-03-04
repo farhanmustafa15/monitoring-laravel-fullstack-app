fd387da1c092b1d6c64c2105eb944ee3601c2100
Farhan <farhanmustafa1506@gmail.com>
2024-03-04 22:24:46 +0700

    style(theme. plugins) add custom colors, grid coloumn, add plugin flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,20 @@ export default {
         "./node_modules/flowbite/**/*.js",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "primary-color": "#0561FC",
+                "white-color": "#FCFCFC",
+            },
+            gridTemplateColumns: {
+                "mentoring-layout": "273px 1167px",
+            },
+        },
     },
 
-    plugins: [require("flowbite/plugin")],
+    plugins: [
+        require("flowbite/plugin")({
+            charts: true,
+        }),
+    ],
 };

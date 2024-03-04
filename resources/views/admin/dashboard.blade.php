796ed62789138e9ba7df265bfac6c5120fbc6bdf
riikuid <fahmibambe@gmail.com>
2024-03-04 18:27:37 +0700

    feat(boilerplate): Laravel@10

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('title')
+    Belly
+@endsection
+
+@section('content')
+    <div class="text-white">test dashboard</div>
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    {{-- Tailwindcss@3.4.1 --}}
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    <title>Login | Monitoring</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles -->
+
+</head>
+
+<body class="bg-black">
+    <p class="text-6xl font-bold text-white">@yield('title')</p>
+    @yield('content')
+</body>
+
+</html>

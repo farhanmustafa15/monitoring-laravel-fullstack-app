427e7169162eda5facb8c964aabef28030547c3e
bforbilly24 <halimputra0701@gmail.com>
2024-03-05 19:54:59 +0700

    refac(file): add script cdn apexcharts, styling backgroun color and dark color

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,11 +3,13 @@
 @section('header')
 @endsection
 
-<body class="w-[1440px] h-full grid grid-cols-mentoring-layout bg-[#F8FAFB] mx-auto ">
-
-    @include('components.sidebar')
-    @yield('layout')
-    <script src="{{ asset('js/graph.js') }}"></script>
+<body class="w-full h-full bg-[#F8FAFB] dark:bg-gray-800 mx-auto ">
+    @include('components.navbar')
+    <div class="grid sm:grid-cols-xl-mentoring-layout xl:grid-cols-mentoring-layout justify-center pt-16">
+        @include('components.sidebar')
+        @yield('layout')
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 </body>
 
 </html>

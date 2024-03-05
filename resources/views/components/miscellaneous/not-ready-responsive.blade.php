cda35a505875fc27b9c4a4e6b6e3365a13e10731
bforbilly24 <halimputra0701@gmail.com>
2024-03-05 22:40:14 +0700

    test(testing): testing commit

--- resources/views/components/miscellaneous/not-ready-responsive.blade.php
+++ resources/views/components/miscellaneous/not-ready-responsive.blade.php
@@ -1,8 +1,8 @@
 @yield('not-ready-responsive')
-<div
-    className='w-full h-full flex flex-col items-center justify-center my-auto p-5 lg:p-10 xl:hidden'>
+{{-- <div
+    className='w-full h-full flex flex-col items-center justify-center my-auto p-5 lg:p-10 xl:hidden'> --}}
     <p class="text-black dark:text-white-color text-center">[PERHATIAN]</p>
     <p class="text-black dark:text-white-color text-center">Harap gunakan perangkat desktop alih-alih menggunakan
         ponsel cerdas, tablet, atau desktop kecil untuk tujuan
         pengalaman pengguna yang terbaik!</p>
-</div>
+{{-- </div> --}}

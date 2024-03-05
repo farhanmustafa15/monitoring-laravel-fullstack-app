03a15be3176f81a1625f3c77792ef00add25a5c7
bforbilly24 <halimputra0701@gmail.com>
2024-03-05 19:56:13 +0700

    refac(components): add script for toggle dark mode

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     {{-- Tailwindcss@3.4.1 --}}
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/css/app.css','resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Monitoring</title>
 
     <!-- Fonts -->
@@ -14,7 +14,13 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
     <!-- Styles -->
-
+    <script>
+        // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia(
+                '(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark')
+        }
+    </script>
 </head>
-
-

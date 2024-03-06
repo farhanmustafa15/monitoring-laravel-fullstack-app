0f224b368f789faab1baf81385e3a99f88ec48eb
Farhan <farhanmustafa1506@gmail.com>
2024-03-06 22:03:39 +0700

    style(file): add responsive xl

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,12 +3,17 @@
 @section('header')
 @endsection
 
-<body class="w-full h-full bg-[#F8FAFB] dark:bg-gray-800 mx-auto ">
-    @include('components.navbar')
-    <div class="grid sm:grid-cols-xl-mentoring-layout xl:grid-cols-mentoring-layout justify-center pt-16">
-        @include('components.sidebar')
-        @yield('layout')
+<body>
+    {{-- Hide this block on extra-large screens --}}
+    <div class="hidden xl:block w-full h-full bg-[#F8FAFB] dark:bg-gray-800 mx-auto">
+        @include('components.navbar')
+        <div class="grid sm:grid-cols-xl-mentoring-layout xl:grid-cols-mentoring-layout justify-center pt-16 bg-[#F8FAFB] dark:bg-gray-800">
+            @include('components.sidebar')
+            @yield('layout')
+        </div>
     </div>
+    @include('components.miscellaneous.not-ready-responsive')
+
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 </body>
 

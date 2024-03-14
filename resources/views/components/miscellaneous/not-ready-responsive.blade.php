2aef84214470132bd1a051f60c2e5f5cca2d74ad
bforbilly24 <halimputra0701@gmail.com>
2024-03-14 20:02:48 +0700

    refac(components): update content of tag p

--- resources/views/components/miscellaneous/not-ready-responsive.blade.php
+++ resources/views/components/miscellaneous/not-ready-responsive.blade.php
@@ -1,6 +1,6 @@
 @yield('not-ready-responsive')
 <div class="xl:hidden bg-gray-800 text-black dark:text-white-color text-center flex flex-col h-full w-full justify-center items-center gap-y-4">
     <p class="font-bold text-black dark:text-white-color text-center">[PERHATIAN]</p>
-    <p class="font-bold text-black dark:text-white-color text-center">Harap gunakan perangkat desktop agar
-        pengalaman pengguna yang terbaik!</p>
+    <p class="font-bold text-black dark:text-white-color text-center">"Harap gunakan perangkat desktop agar
+        pengalaman pengguna yang terbaik!"</p>
 </div>

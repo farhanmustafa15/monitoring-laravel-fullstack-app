29e70ebf35079fe521ab2a38b5984ff689288ddf
bforbilly24 <halimputra0701@gmail.com>
2024-03-26 12:18:45 +0700

    feat(page): add resources/js/dashboard

--- resources/js/dashboard.js
+++ resources/js/dashboard.js
@@ -1,18 +1,64 @@
-document.getElementById('rumahJamurButton').addEventListener('click', function() {
-    // Make an AJAX request to fetch the Rumah Jamur data
-    // Update the line chart with the fetched data
-    let labels = ['avgt', 'avgh']; // Example labels
-    let data = [25, 30]; // Example data
-    updateLineChart(labels, data);
+var ctx = document.getElementById('lineChart').getContext('2d');
+var lineChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['RumahJamur', 'TugasAkhir'],
+        datasets: [{
+            label: 'avgt',
+            data: [27, 26.2],
+            borderColor: 'rgb(255, 99, 132)',
+            borderWidth: 1,
+            fill: false
+        }, {
+            label: 'avgh',
+            data: [55, 69.75],
+            borderColor: 'rgb(54, 162, 235)',
+            borderWidth: 1,
+            fill: false
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
 });
 
-document.getElementById('tugasAkhirButton').addEventListener('click', function() {
-    // Make an AJAX request to fetch the Tugas Akhir data
-    // Update the line chart with the fetched data
-    let labels = ['avgt', 'avgh']; // Example labels
-    let data = [30, 35]; // Example data
-    updateLineChart(labels, data);
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById('rumahJamurButton').addEventListener('click', function() {
+//     // Make an AJAX request to fetch the Rumah Jamur data
+//     // Update the line chart with the fetched data
+//     let labels = ['avgt', 'avgh']; // Example labels
+//     let data = [25, 30]; // Example data
+//     updateLineChart(labels, data);
+// });
+
+// document.getElementById('tugasAkhirButton').addEventListener('click', function() {
+//     // Make an AJAX request to fetch the Tugas Akhir data
+//     // Update the line chart with the fetched data
+//     let labels = ['avgt', 'avgh']; // Example labels
+//     let data = [30, 35]; // Example data
+//     updateLineChart(labels, data);
+// });
 
 
 

0b7c03a585266efee17d4317bb0b766321da4891
bforbilly24 <halimputra0701@gmail.com>
2024-04-30 22:41:41 +0700

    refactor(page): update graph for linegraph avgt and linegraph avgh

--- resources/js/dashboard.js
+++ resources/js/dashboard.js
@@ -1,52 +1,87 @@
-document.addEventListener('DOMContentLoaded', function() {
+// Avgt
+document.addEventListener("DOMContentLoaded", function () {
     var url = window.location.href;
-    var dataType = url.split('/').pop();
+    var dataType = url.split("/").pop();
 
-    fetch('/api/data/' + dataType)
-    .then(response => response.json())
-    .then(data => {
-        var labels = [];
-        var avgtData = [];
-        var avghData = [];
+    fetch("/api/data/" + dataType)
+        .then((response) => response.json())
+        .then((data) => {
+            var labels = [];
+            var avgtData = [];
 
-        // Iterate over each object in the JSON data
-        Object.keys(data).forEach(key => {
-            var item = data[key];
-            labels.push(key); // Assuming 'key' is the label, like '1', '2', etc.
-            avgtData.push(item.avgt);
-            avghData.push(item.avgh);
-        });
+            Object.keys(data).forEach((key) => {
+                var item = data[key];
+                labels.push(key);
+                avgtData.push(item.avgt);
+            });
 
-        var ctx = document.getElementById('lineChart').getContext('2d');
-        var lineChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: labels, // Use the 'labels' array constructed above
-                datasets: [
-                    {
-                        label: 'avgt',
-                        data: avgtData,
-                        borderColor: 'rgb(255, 99, 132)',
-                        borderWidth: 1,
-                        fill: false
+            var ctx = document.getElementById("lineChartAvgt").getContext("2d");
+            var lineChart = new Chart(ctx, {
+                type: "line",
+                data: {
+                    labels: labels,
+                    datasets: [
+                        {
+                            label: "avgt",
+                            data: avgtData,
+                            borderColor: "rgb(255, 99, 132)",
+                            borderWidth: 1,
+                            fill: false,
+                        },
+                    ],
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true,
+                        },
                     },
-                    {
-                        label: 'avgh',
-                        data: avghData,
-                        borderColor: 'rgb(54, 162, 235)',
-                        borderWidth: 1,
-                        fill: false
-                    }
-                ]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-    })
-    .catch(error => console.error('Error:', error));
+                },
+            });
+        })
+        .catch((error) => console.error("Error:", error));
+});
+
+// Avgh
+document.addEventListener("DOMContentLoaded", function () {
+    var url = window.location.href;
+    var dataType = url.split("/").pop();
+
+    fetch("/api/data/" + dataType)
+        .then((response) => response.json())
+        .then((data) => {
+            var labels = [];
+            var avghData = [];
+
+            Object.keys(data).forEach((key) => {
+                var item = data[key];
+                labels.push(key);
+                avghData.push(item.avgh);
+            });
+
+            var ctx = document.getElementById("lineChartAvgh").getContext("2d");
+            var lineChart = new Chart(ctx, {
+                type: "line",
+                data: {
+                    labels: labels,
+                    datasets: [
+                        {
+                            label: "avgh",
+                            data: avghData,
+                            borderColor: "rgb(54, 162, 235)",
+                            borderWidth: 1,
+                            fill: false,
+                        },
+                    ],
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true,
+                        },
+                    },
+                },
+            });
+        })
+        .catch((error) => console.error("Error:", error));
 });

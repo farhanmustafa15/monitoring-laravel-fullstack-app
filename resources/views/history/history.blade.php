ccbac6207c9f0758c7524d934ca7dc043d03e85c
bforbilly24 <halimputra0701@gmail.com>
2024-05-26 15:52:29 +0700

    refactor(page): update table mapping data

--- resources/views/history/history.blade.php
+++ resources/views/history/history.blade.php
@@ -2,14 +2,14 @@
 
 @section('layout')
     {{-- History --}}
-    <div class="pt-8 mx-6 w-full">
+    <div class="pt-8 mx-6 w-full h-screen">
 
         {{-- Choose database --}}
         <div class="flex justify-end px-6 mb-5">
             <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown"
                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm rounded-lg w-56 py-2.5 justify-center text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                type="button">Pilih Database<svg class="w-2.5 h-2.5 ms-3" aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                type="button">Pilih Database<svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                    fill="none" viewBox="0 0 10 6">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="m1 1 4 4 4-4" />
                 </svg>
@@ -34,9 +34,10 @@
         </div>
 
         <div class="flex flex-col md:flex-row gap-10">
-            {{-- Temperatur --}}
+            {{-- temperature --}}
             <div class="w-full h-[500px] flex flex-col gap-y-4">
                 <h1 class="text-2xl font-bold text-black dark:text-white-color">Suhu</h1>
+
                 <div class="relative overflow-x-auto sm:rounded-lg w-full shadow-sm shadow-blue-600 dark:shadow-white">
                     <div class="relative overflow-x-auto">
                         <table
@@ -58,29 +59,36 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach ($avgtData as $index => $value)
-                                    <tr class="grid grid-cols-2">
-                                        <td class="px-6 py-3 flex flex-col bg-white dark:bg-gray-800 border-r border-b dark:border-gray-700 border-gray-200 items-center justify-center">
-                                            <div class="py-1.5 w-full text-center font-medium text-base text-gray-900 whitespace-nowrap dark:text-white-color">
-                                                {{ $index }}
-                                            </div>
-                                        </td>
-                                        <td class="px-6 py-3 flex flex-col bg-white dark:bg-gray-800 border-r border-b dark:border-gray-700 border-gray-200 items-center justify-center">
-                                            <div class="py-1.5 w-full text-center font-medium text-base text-gray-900 whitespace-nowrap dark:text-white-color">
-                                                {{ $value }}&percnt;
-                                            </div>
-                                        </td>
-                                    </tr>
-                                @endforeach
+                                @isset($avgtData)
+                                    @foreach ($avgtData as $key => $value)
+                                        <tr class="grid grid-cols-2">
+                                            <td
+                                                class="px-6 py-3 bg-white dark:bg-gray-800 border-r border-b dark:border-gray-700 border-gray-200 flex items-center justify-center">
+                                                <p
+                                                    class="text-center font-medium text-base text-gray-900 whitespace-nowrap dark:text-white-color">
+                                                    {{ $key }}
+                                                </p>
+                                            </td>
+                                            <td
+                                                class="px-6 py-3 bg-white dark:bg-gray-800 border-r border-b dark:border-gray-700 border-gray-200 flex items-center justify-center">
+                                                <p
+                                                    class="text-center font-medium text-base text-gray-900 whitespace-nowrap dark:text-white-color">
+                                                    {{ $value }}&percnt;
+                                                </p>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                @endisset
                             </tbody>
                         </table>
                     </div>
                 </div>
             </div>
 
-            {{-- Humidity --}}
+            {{-- humidity --}}
             <div class="w-full h-[500px] flex flex-col gap-y-4">
                 <h1 class="text-2xl font-bold text-black dark:text-white-color">Kelembapan</h1>
+
                 <div class="relative overflow-x-auto sm:rounded-lg w-full shadow-sm shadow-blue-600 dark:shadow-white">
                     <div class="relative overflow-x-auto">
                         <table
@@ -102,20 +110,26 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach ($avghData as $index => $value)
-                                    <tr class="grid grid-cols-2">
-                                        <td class="px-6 py-3 flex flex-col bg-white dark:bg-gray-800 border-r border-b dark:border-gray-700 border-gray-200 items-center justify-center">
-                                            <div class="py-1.5 w-full text-center font-medium text-base text-gray-900 whitespace-nowrap dark:text-white-color">
-                                                {{ $index }}
-                                            </div>
-                                        </td>
-                                        <td class="px-6 py-3 flex flex-col bg-white dark:bg-gray-800 border-r border-b dark:border-gray-700 border-gray-200 items-center justify-center">
-                                            <div class="py-1.5 w-full text-center font-medium text-base text-gray-900 whitespace-nowrap dark:text-white-color">
-                                                {{ $value }}&percnt;
-                                            </div>
-                                        </td>
-                                    </tr>
-                                @endforeach
+                                @isset($avghData)
+                                    @foreach ($avghData as $key => $value)
+                                        <tr class="grid grid-cols-2">
+                                            <td
+                                                class="px-6 py-3 bg-white dark:bg-gray-800 border-r border-b dark:border-gray-700 border-gray-200 flex items-center justify-center">
+                                                <p
+                                                    class="text-center font-medium text-base text-gray-900 whitespace-nowrap dark:text-white-color">
+                                                    {{ $key }}
+                                                </p>
+                                            </td>
+                                            <td
+                                                class="px-6 py-3 bg-white dark:bg-gray-800 border-r border-b dark:border-gray-700 border-gray-200 flex items-center justify-center">
+                                                <p
+                                                    class="text-center font-medium text-base text-gray-900 whitespace-nowrap dark:text-white-color">
+                                                    {{ $value }}&percnt;
+                                                </p>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                @endisset
                             </tbody>
                         </table>
                     </div>

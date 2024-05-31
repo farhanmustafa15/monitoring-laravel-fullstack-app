67de58785003f91c9ba14acf2b6d0463f70de972
bforbilly24 <halimputra0701@gmail.com>
2024-05-31 21:51:40 +0700

    refactor(config): add third party firebase

--- config/services.php
+++ config/services.php
@@ -31,4 +31,10 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'firebase' => [
+        'credentials' => env('FIREBASE_CREDENTIALS'),
+        'database_url' => env('FIREBASE_DATABASE_URL'),
+    ],
+
+
 ];
